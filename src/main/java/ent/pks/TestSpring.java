f76c27bcf46ede3fcfb0cb9dce98196008a73cc4
Kirill <kirill.peshin@gmail.com>
2020-03-18 05:57:41 +0300

    L7 end

--- src/main/java/ent/pks/TestSpring.java
+++ src/main/java/ent/pks/TestSpring.java
@@ -7,15 +7,9 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        PopMusic popMusic = context.getBean("popBean", PopMusic.class);
-//        RockMusic rockMusic = context.getBean("rockBean", RockMusic.class);
-//        ClassicalMusic classicalMusic = context.getBean("classicBean", ClassicalMusic.class);
-//
-//        musicPlayer.setMusicList(popMusic);
-//        musicPlayer.setMusicList(rockMusic);
-//        musicPlayer.setMusicList(classicalMusic);
 
         musicPlayer.playMusic();
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
